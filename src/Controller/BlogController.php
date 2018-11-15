e5c5e534a2aa21deb59b8be163a331c8a35e87c7
Laura Lenik <Laura.Lenik78@gmail.com>
2018-11-15 16:31:45 +0100

    php bin/console make:form

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -64,4 +64,4 @@ class BlogController extends AbstractController
 		}
 	}
 
-
+	
--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Form\CategoryType;
 use App\Entity\Category;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,11 +13,22 @@ class CategoryController extends AbstractController
     /**
      * @Route("/category", name="category")
      */
-    public function index()
+    public function index(Request $request)
     {
 	
 	    $category = new Category();
 	    $form = $this->createForm(CategoryType::class, $category);
+	    $form->handleRequest($request);
+	    
+	    
+	    if($form->isSubmitted() && $form->isValid())
+	    {
+	        $category = $form->getData();
+	        $EntityManager = $this->getDoctrine()->getManager();
+	        $EntityManager->persist($category);
+	        $EntityManager->flush();
+	    }
+	    
 	    
         return $this->render('category/index.html.twig', [
             'controller_name' => 'CategoryController',
